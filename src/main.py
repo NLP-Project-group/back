881844250938aaf7b406a4727ce430f58fa404c8
Aris <solonantenainageorgino@gmail.com>
2025-08-02 10:41:39 +0200

    first_commit_traduction

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-# Point d’entrée global
+import sys
+import os
+
+# Ajouter le dossier 'translation' au path Python
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "translation"))
+if module_path not in sys.path:
+    sys.path.insert(0, module_path)
+
+from traduction import main
+
+if __name__ == "__main__":
+    print("🟢 Lancement du programme de traduction...")
+    main()
